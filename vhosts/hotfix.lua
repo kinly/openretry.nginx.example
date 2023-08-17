81bad613478d60a7966a91e6af9c55684f59dabb
kinly <kinly.lei@gmail.com>
2023-08-17 20:17:35 +0800

    Add files via upload

--- vhosts/hotfix.lua
+++ vhosts/hotfix.lua
@@ -2,21 +2,21 @@ local whiteid = {'1000','1001','1002','1003'}
 local whiteip = {'192.168.','192.167.','1.1.1.1','2.2.2.2'}
 
 function iswhiteid(whiteid, val)
-	for _, v in ipairs(whiteid) do
-		if v == val then
-			return 1
-		end
-	end
-	return 0
+    for _, v in ipairs(whiteid) do
+        if v == val then
+            return 1
+        end
+    end
+    return 0
 end
 
 function iswhiteip(whiteip, val)
-	for _, v in ipairs(whiteip) do
-		if string.find(val, v) ~= nil then
-			return 1
-		end
-	end
-	return 0
+    for _, v in ipairs(whiteip) do
+        if string.find(val, v) ~= nil then
+            return 1
+        end
+    end
+    return 0
 end
 
 local headers_tab = ngx.req.get_headers()
@@ -24,26 +24,26 @@ local ip=headers_tab["X-REAL-IP"] or headers_tab["X_FORWARDED_FOR"] or ngx.var.r
 if headers_tab["acountid"] ~= nil then
     id = headers_tab["acountid"]
     if iswhiteid(whiteid,id) == 1 then
-		ngx.exec("@hotfix-white");
+        ngx.exec("@hotfix-white");
     else
-		if iswhiteip(whiteip,ip) == 1 then
-			ngx.exec("@hotfix-white")
-			--ngx.say(ip)
-		else
-			ngx.exec("@hotfix-common")
-			--ngx.say(ip)
-		end
+        if iswhiteip(whiteip,ip) == 1 then
+            ngx.exec("@hotfix-white")
+            --ngx.say(ip)
+        else
+            ngx.exec("@hotfix-common")
+            --ngx.say(ip)
+        end
     end
 else
     if iswhiteip(whiteip,ip) == 1 then
-		ngx.exec("@hotfix-white")
+        ngx.exec("@hotfix-white")
     --ngx.say("head is null")
-	--ngx.exec("@hotfix-common");
-	--ngx.say("xip: ",headers_tab["X-REAL-IP"])
+    --ngx.exec("@hotfix-common");
+    --ngx.say("xip: ",headers_tab["X-REAL-IP"])
     --ngx.say("fip: ",headers_tab["X_FORWARDED_FOR"])
     --ngx.say("rip: ",ngx.var.remote_addr)
     --ngx.say(ip)
-	else
-		ngx.exec("@hotfix-common")
-	end
+    else
+        ngx.exec("@hotfix-common")
+    end
 end
